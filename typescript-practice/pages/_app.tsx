46e0beb5495ac97a814634e4deb8275dfba65be2
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-09-10 00:44:08 +0000

    Made dogs appear in photo container

--- typescript-practice/pages/_app.tsx
+++ typescript-practice/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FaBeer } from "react-icons/fa";
+import Link from 'next/link';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return(
 <Component {...pageProps} />
--- typescript-practice/pages/index.tsx
+++ typescript-practice/pages/index.tsx
@@ -3,65 +3,48 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
-import david from "./Img.Container";
+import Header from "./api/header";
+import { FaPaw } from "react-icons/fa";
 
 const Home = (res: any) => {
-  async function handleClick(event: any) {
+  const [image, handleImage] = useState('')
+  function handleClick(res: any): any {
+    const fetchPromise = fetch("https://random.dog/woof.json")
+    fetchPromise.then(response => {
+      return response.json()
+    }).then(data => handleImage(data.url))
 
-fetch('https://random.dog/woof.json')
-.then(res => (console.log(res.json())))
   }
 
   return (
     <>
-  <header>
-    <div className="row header">
-      <h1>Dog Generator<a className="home">Home</a><a className="favorites-link">Favorites</a></h1>
-    </div>
-  </header>
-   <div className="container">
-      <div className="row generate-img">
-        <div className="generate-img">
-          <button className="bigbutton" onLoad={handleClick} onClick={handleClick}>Get Random Dog Image</button>
-        </div>
-        {/* <div className="lds-spinner hidden">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div> */}
-      </div>
-      <div data-view="home-page" className="row image-container">
-        <img src="https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png" className="image" id="photoUrl"/>
-        <i className="fa-solid fa-paw fa-2x icon"></i>
-      </div>
-      </div>
-      {/* <h2 className="favorites-view-text hidden">Favorites</h2> */}
-
-{/* <div data-view="favorites" class="favorites hidden row">
-        <form class="form">
-          <div>
-            <input type="submit" class="save-btn hidden" id="button" value="SAVE">
+      <Header />
+      <div className="container">
+        <div className="row generate-img">
+          <div className="generate-img">
+            <button
+              className="bigbutton"
+              onLoad={handleClick}
+              onClick={handleClick}
+            >
+              Get Random Dog Image
+            </button>
           </div>
-        </form>
+        </div>
+        <div data-view="home-page" className="row image-container">
+          <img
+
+            src={image}
+            className="image"
+            id="photoUrl"
+          />
+          <i>
+            <FaPaw />
+          </i>
+        </div>
       </div>
-    </div>
-    <div id="overlay" class="hidden overlay">
-      <div id="modal" class="hidden">
-        <h4>Are you sure you want to delete this entry?</h4>
-        <a id="confirm-modal" class="confirm-modal">CONFIRM</a>
-        <a id="close-modal-btn" class="close-modal-btn">CANCEL</a>
-      </div>
-    </div> */}
-        </>
+
+    </>
   );
 };
 
